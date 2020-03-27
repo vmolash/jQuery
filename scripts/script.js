416fe8f5769c922b00d99677639e9441d9652602
vmolash <molash.v@gmail.com>
2020-03-26 21:24:16 -0700

    removeClass

--- scripts/script.js
+++ scripts/script.js
@@ -18,4 +18,5 @@
 // multiple class and elements
 $(document).ready(function() {
         $('ul.numbers li:even, ul.colors li:odd').addClass('striped underline');
+        $('ul.numbers li:first').removeClass('striped');
 });
